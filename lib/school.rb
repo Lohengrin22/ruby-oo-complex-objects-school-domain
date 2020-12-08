ffad4156a80cedd8ffb2c8a4a3bc798cfde1a0f8
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-12-08 17:08:00 -0500

    commit tree 104b92ccd8bc1f2133ec6359dbc2bf4c9228f22d

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+require 'pry'
+class School
+    attr_reader :roster, :name, :grade 
+   
+    def initialize(name)    
+        @name = name    
+        @roster = {}  
+    end
+   
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name  
+    end
+
+def grade(grade)
+
+return @roster[grade].sort
+
+end 
+
+def sort
+sort_hash = {}
+@roster.each do |grade, students| 
+    sort_hash[grade] = students.sort
+end
+sort_hash
+end 
+ end
